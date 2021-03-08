05532537c59e04de612b8f00dd20543503546ff2
Cleiton Oliveira <cleiton.ti@outlook.com>
2021-03-08 17:33:41 -0300

    init

---
+++ src/main/java/br/com/zherro/springmundi/SpringMundiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zherro.springmundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMundiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zherro/springmundi/SpringMundiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zherro.springmundi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMundiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
